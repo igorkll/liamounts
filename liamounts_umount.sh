80dcb15a2bdecff6e79dbf0684a0774220cd9fa2
igorkll <frerther@gmail.com>
2026-03-17 16:59:29 +0300

    Update liamounts_umount.sh

--- liamounts_umount.sh
+++ liamounts_umount.sh
@@ -2,26 +2,9 @@
 
 exec >> /var/log/liamounts.log 2>&1
 
-echo "liamounts umount"
+PART="$1"
+echo "liamounts umount: $PART"
 
-for dir in /automounts/*; do
-    [ -d "$dir" ] || continue
-    
-    echo "check for umount: $dir"
-    if ! ls "$dir" >/dev/null 2>&1; then
-        umount -l "$dir"
-        rmdir "$dir"
-    fi
-done
 
-for dir in /realmounts/*; do
-    [ -d "$dir" ] || continue
-    
-    echo "check for umount: $dir"
-    if ! ls "$dir" >/dev/null 2>&1; then
-        umount -l "$dir"
-        rmdir "$dir"
-    fi
-done
 
 echo "umount completed!"
