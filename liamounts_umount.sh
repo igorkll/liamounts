505bc49eca0a5149f1babab14d8612448c8920e6
igorkll <frerther@gmail.com>
2026-03-17 17:06:24 +0300

    Update liamounts_umount.sh

--- liamounts_umount.sh
+++ liamounts_umount.sh
@@ -5,6 +5,8 @@ exec >> /var/log/liamounts.log 2>&1
 PART="$1"
 echo "liamounts umount: $PART"
 
-
+findmnt -l -o TARGET --source "$PART" -n | while read path; do
+    echo "$path"
+done
 
 echo "umount completed!"
