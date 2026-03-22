d93b12c8de41576a0bb10a5ea52a16ee336a4eeb
igorkll <frerther@gmail.com>
2026-03-23 02:32:08 +0300

    Update liamountsctl.c

--- liamountsctl/liamountsctl.c
+++ liamountsctl/liamountsctl.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
 #include <string.h>
 #include <mntent.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-static void showMount(const char* name, const char* path, const char* file) {
-    printf("%s %s %s\n", name, path, file);
+bool fs_exists(const char* path) {
+    return access(path, F_OK) == 0;
 }
 
-static void iterateMounts(void(*callback)(const char* name, const char* path, const char* file)) {
+static void showMount(const char* name, const char* file, const char* access_mount_directory, const char* real_mount_directory) {
+    printf("%s %s %s %s\n", name, file, access_mount_directory, real_mount_directory);
+}
+
+static void iterateMounts(void(*callback)(const char* name, const char* file, const char* access_mount_directory, const char* real_mount_directory)) {
     FILE *mounts = setmntent("/proc/mounts", "r");
     if (!mounts) {
         perror("setmntent");
@@ -20,19 +28,19 @@ static void iterateMounts(void(*callback)(const char* name, const char* path, co
         const char* target_dir = "/automounts/";
         
         if (strncmp(entry->mnt_dir, target_dir, strlen(target_dir)) == 0) {
-            printf("1  %s -> %s (%s)\n", 
-                entry->mnt_fsname, 
-                entry->mnt_dir,
-                entry->mnt_type);
-        } else {
-            target_dir = "/realmounts/";
+            char* real_mount_directory = strdup(entry->mnt_dir);
+            if (real_mount_directory) {
+                const char* str = "/realmounts/";
+                memcpy(real_mount_directory, str, strlen(str));
 
-            if (strncmp(entry->mnt_dir, target_dir, strlen(target_dir)) == 0) {
-                printf("2  %s -> %s (%s)\n", 
-                    entry->mnt_fsname, 
-                    entry->mnt_dir,
-                    entry->mnt_type);
+                if (fs_exists(real_mount_directory)) {
+                    callback("", "", entry->mnt_dir, real_mount_directory);
+                } else {
+                    callback("", "", entry->mnt_dir, real_mount_directory);
+                }
             }
+
+            free(real_mount_directory);
         }
     }
     
@@ -45,7 +53,6 @@ int main(int argc, char* argv[]) {
         printf("liamountsctl umount name - \n");
         return 0;
     }
-
 
     if (strcmp(argv[1], "list") == 0) {
         iterateMounts(showMount);
