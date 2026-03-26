e6c329a36ac7c7aed34e91ee8210c3cd18da316c
igorkll <frerther@gmail.com>
2026-03-26 22:24:25 +0300

    Update build.sh

--- liamountsctl/build.sh
+++ liamountsctl/build.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Restart with root..."
+    exec sudo "$0" "$@"
+    exit $?
+fi
+
 gcc liamountsctl.c -o liamountsctl
-sudo chown root:root liamountsctl
-sudo chmod +s liamountsctl # yes... is suid
+chown root:root liamountsctl
+chmod +s liamountsctl # yes... is suid
